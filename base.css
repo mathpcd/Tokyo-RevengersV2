624d81e12f7b7cbe25874809706c380a0a04017a
mathpcd <84201298+mathpcd@users.noreply.github.com>
2021-05-15 00:44:35 +0200

    Initial commit

---
+++ base.css
@@ -0,0 +1,639 @@
+.userPopout-3XzG_A {
+  box-shadow: none;
+  border-radius: 5px;
+  background-size: cover;
+  background: url(https://ibb.co/16M4Ry2) 20%/cover;
+  ;
+}
+
+.popout-2iWAc- {
+  box-shadow: none;
+}
+
+.headerPlaying-j0WQBV {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.topSectionPlaying-1J5E4n {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.headerSpotify-zpWxgT,
+.headerXbox-3G-4PF {
+  background: rgba(18, 96, 45, 0.598);
+}
+
+.topSectionXbox-3fWLjS,
+.topSectionSpotify-1lI0-P {
+  background: rgba(18, 160, 15, 0.65);
+}
+
+.headerStreaming-2FjmGz {
+  background: rgba(65, 20, 113, 0.782);
+}
+
+.topSectionStreaming-1Tpf5X {
+  background: rgba(65, 20, 113, 0.80);
+}
+
+
+.theme-dark .body-3iLsc4,
+.theme-dark .bodyInner-245q0L,
+.theme-dark .footer-1fjuF6 {
+  background-color: rgba(0, 0, 0, 0.47);
+  color: white;
+}
+
+.theme-dark .footer-1fjuF6 {
+  background-color: rgba(0, 0, 0, 0.67);
+}
+
+.body-3ND3kc {
+  background-color: rgba(0, 0, 0, 0.69);
+}
+
+.theme-dark .footer-1fjuF6 {
+  border-color: transparent;
+}
+
+.bodyTitle-Y0qMQz,
+.userInfoSectionHeader-CBvMDh {
+  font-weight: 400;
+  text-transform: capitalize;
+  color: silver;
+}
+
+.note-3kmerW textarea:focus {
+  background-color: rgba(255, 255, 255, .094);
+}
+
+.note-3HfJZ5 textarea,
+.note-QfFU8y textarea {
+  border-radius: 7px;
+}
+
+.note-3kmerW textarea {
+  resize: none;
+  border: none;
+  font-size: 12px;
+  line-height: 4px;
+  padding: 9px 7px;
+  width: 100%;
+  color: white;
+}
+
+.theme-dark .quickMessage-1yeL4E {
+  background-color: rgba(36, 37, 38, 0.78);
+  border-color: transparent;
+  color: white;
+}
+
+.quickMessage-2XpSaN {
+  border-radius: 7px;
+}
+
+.theme-dark .actionButton-VzECiy {
+  border-color: #4f545c;
+  color: white;
+  background: #4f545c;
+}
+
+.theme-dark .container-VSDcQc .sectionTag-pXyto9 {
+  background-color: black;
+  color: white;
+}
+
+.protip-YaFfgO {
+  display: none;
+}
+
+.tabBarContainer-1s1u-z {
+  border-top: none;
+}
+
+.userInfoSection-2acyCx+.userInfoSection-2acyCx {
+  border-top: none;
+}
+
+.connectedAccounts-repVzS {
+  margin-top: -40px;
+}
+
+.connectedAccount-36nQx7 {
+  border-radius: 12px;
+  border: 2px solid #141414;
+}
+
+.connectedAccount-36nQx7 {
+  background-color: rgba(0, 0, 0, 0.32);
+}
+
+.listRow-hutiT_ {
+  color: white;
+  border-radius: 11px;
+  margin: 1px 0 12px 12px;
+}
+
+.listRow-hutiT_:hover {
+  background-color: rgba(0, 0, 0 , 0.408);
+  border: 1px solid rgba(0, 0, 0 , 0.76);
+}
+
+.members-1998pB {
+  width: 240px;
+  background-color: rgba(0, 0, 0, 0.33);
+}
+
+.members-1998pB>div {
+  background-color: transparent;
+}
+
+.activity-E-0FeD {
+  color: white;
+}
+
+.theme-dark .description-2QALGo {
+  color: white;
+}
+
+.theme-dark .autocomplete-1vrmpx {
+  background: linear-gradient(135deg, #808080 0%, rgba(0, 0, 0, 0.95) 100%);
+}
+
+.mentioned-xhSam7:before {
+  display: none;
+}
+
+.theme-dark .mentioned-xhSam7,
+.message-2qnXI6.mentioned-xhSam7.selected-2P5D_Z,
+.mouse-mode.full-motion .mentioned-xhSam7:hover {
+  background: none;
+}
+
+.mentioned-xhSam7 {
+  border-radius: 8px;
+}
+
+.mentioned-xhSam7 {
+  padding: .2rem .2rem;
+  margin: .2rem .1rem;
+}
+
+.theme-dark .mentioned-xhSam7 .mention {
+  background: rgba(17, 17, 17, 0.8) !important;
+  padding: .1rem .4rem;
+  color: white !important;
+  border: none;
+  border-radius: 3px;
+}
+
+.theme-dark .mentioned-xhSam7 .mention:hover {
+  background: rgba(17, 17, 17, 0.6) !important;
+}
+
+.theme-dark .wrapper-3WhCwL {
+  color: white !important;
+  background-color: rgba(0, 0, 0 , 0.36);
+  padding: .1rem .4rem;
+  font-weight: 400;
+}
+
+.theme-dark .wrapper-3WhCwL:hover {
+  background-color: rgba(0, 0, 0 , 0.56);
+  border: none;
+}
+
+.markup-2BOw-j code {
+  font-size: 0.975rem;
+  border: none;
+  background: linear-gradient(135deg, rgba(0, 0, 0 , 0.51) 0%, rgba(0, 0, 0, 0.77) 100%);
+}
+
+.detailsBlock-FoDTGA {
+  background: none;
+}
+
+.hljs {
+  padding: .5em;
+  color: white;
+}
+
+.markup-2BOw-j pre {
+  border: none;
+  background: none;
+}
+
+.markup-2BOw-j code.inline {
+  padding: .5em;
+}
+
+.theme-dark .wrapper-2qzCYF {
+  background-color: rgba(0, 0, 0, 0.26);
+}
+
+.status-1WEaea.voice-3bPYC- {
+  background-color: #808080;
+}
+
+.theme-dark .quickSelectPopoutOption-opKBx9 {
+  background: black;
+  color: white;
+}
+
+.theme-dark .quickSelectPopoutOption-opKBx9:hover {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.timestamp-1E3uAL,
+.container-3-pyIM,
+.cozy-3raOZG .timestamp-3ZCmNB.alt-1uNpEt {
+  color: white;
+}
+
+.guildIcon-3h-1IH {
+  background-color: transparent;
+  color: white;
+}
+
+.navButtonActive-1MkytQ {
+  background: rgba(0, 0, 0, 0.79);
+  border-left: none;
+  box-shadow: none;
+}
+
+.wrapper-2Gsate {
+  background: none;
+}
+
+.navButtonActive-1MkytQ:hover {
+  background-color: #808080;
+}
+
+.navButtonActive-1MkytQ.active {
+  background-color: #808080 !important;
+  border: 2px solid rgba(255, 255, 255, 0.15);
+}
+
+.contentWrapper-SvZHNd, .emojiPicker-3PwZFl {
+  background: linear-gradient(135deg, #808080 0%, #171717 100%);
+}
+
+.inspector-S2gM3e {
+  background: none;
+}
+
+.emojiItem-14v6tW.emojiItemSelected-1aLkfV {
+  background-color: rgba(0, 0, 0, .4);
+}
+
+.emojiPicker-3PwZFl .header-8ilj5e {
+  border: none;
+  box-shadow: none;
+}
+
+.emojiPicker-3PwZFl .header-8ilj5e .searchBar-2pWH0_ {
+  background-color: transparent;
+}
+
+.contentWrapper-SvZHNd .diversitySelectorPopout-3FiGaM {
+  background: transparent;
+  border-color: transparent;
+}
+
+.wrapper-1-Fsb8 {
+  background-color: transparent;
+}
+
+.colorDefault-2K3EoJ .checkbox-3s5GYZ {
+  color: rgba(0, 0, 0, .26) !important;
+}
+
+.colorDefault-2K3EoJ.focused-3afm-j .check-1JyqgN {
+  color: white;
+}
+
+.categoryItemDefaultCategorySelected-_HCKoz, .categoryItemDefaultCategorySelected-_HCKoz:hover {
+  border-bottom-color: white;
+  border-bottom-width: 3px;
+  border-style: solid;
+  background: none;
+}
+
+.clickable-3rdHwn foreignObject[mask] {
+  mask: none;
+}
+
+.iconBadge-qZ4Ksk {
+  position: absolute;
+  right: 2px;
+  bottom: 1px;
+  width: 10px;
+  height: 10px;
+  border-radius: 8px;
+  background-color: #808080;
+  border: 1px solid rgba(255, 255, 255, 0.41);
+}
+
+.theme-dark .guildName-1Bc3Ta,
+.colorDefault-2K3EoJ {
+  color: rgb(222, 222, 222);
+}
+
+.bg-h5JY_x {
+  background-color: transparent;
+}
+
+.color-primary {
+  color: #808080 rgba(0, 0, 0 , 1.0)
+}
+
+.unreadPill-2HyYtt {
+  border-radius: 0;
+  color: #fff;
+  background-color: #808080;
+}
+
+.theme-dark .theme-dark .form-2fGMdU {
+  background: none;
+}
+
+.unreadPillCapStroke-7rkHbg {
+  color: #808080;
+  fill: #808080;
+}
+
+.theme-dark .theme-dark .form-2fGMdU:before,
+.separator-42rNt0 {
+  background: none !important;
+}
+
+.form-2fGMdU:before {
+  background: none !important;
+}
+
+.icon-KgGMGo,
+.button-1ZiXG9 {
+  color: white;
+  background-color:#808080;
+}
+
+.divider-24xeUg {
+  border: none;
+}
+
+.toolbar-2bjZV7:before {
+  border-top: 8px solid #808080;
+}
+
+.toolbar-2bjZV7 {
+  background-color: #808080;
+  border-radius: 0;
+  box-shadow: none;
+}
+
+.wrapper-2aW0bm {
+  background-color: #808080;
+  border-radius: 10;
+}
+
+.button-1ZiXG9:hover {
+  color: white;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.reactionBtn-3N03Zj {
+  visibility: visible;
+  color: white;
+  height: 1.7rem;
+  width: 1.7rem;
+  margin-left: 6px;
+}
+
+.akaBadge-1M-1Gw {
+  background-color: #808080;
+  color: white;
+}
+
+.channelName-1JRO3C {
+  color: white;
+  background: rgba(0, 0, 0, .3);
+}
+
+.searchResultMessage-1fxgXh.hit-1fVM9e {
+  box-shadow: none;
+  background-color: rgba(0, 0, 0, .3);
+}
+
+.colorDefault-2K3EoJ .radioSelection-1HmrQS {
+  color: #808080 !important;
+}
+
+.live-vBWnV6 {
+  background-color: #808080 !important;
+}
+
+.background-1QDuV2,
+.accountList-33MS45,
+.connection-1fbD7X {
+  background-color: rgba(0, 0, 0, .26);
+}
+
+.fieldList-21DyL8 {
+  background-color: transparent;
+}
+
+.connectionHeader-2MDqhu {
+  background: none;
+}
+
+.integration-3kMeY4 {
+  background-color: transparent;
+}
+
+.scroller-3BxosC:hover {
+  color: #ffffff;
+}
+
+.strikethrough-1n4ekb {
+  color: #808080;
+}
+
+.lookFilled-1Gx00P.colorGrey-2DXtkV {
+  background-color: #141414;
+}
+
+.lookFilled-1Gx00P.colorGrey-2DXtkV:hover {
+  background-color: #141414;
+}
+
+.button-1YfofB.buttonColor-7qQbGO.buttonActive-3FrkXp, .button-1YfofB .buttonColor-7qQbGO.buttonActive-3FrkXp {
+  background-color: #141414;
+}
+
+.button-1YfofB.buttonColor-7qQbGO.buttonActive-3FrkXp:hover, .button-1YfofB .buttonColor-7qQbGO.buttonActive-3FrkXp:hover {
+  background-color: #141414;
+}
+
+.theme-dark .lookFilled-1Gx00P.colorPrimary-3b3xI6 {
+  background-color: #141414 !important;
+}
+
+.theme-dark .lookFilled-1Gx00P.colorPrimary-3b3xI6:hover {
+  background-color: #141414 !important;
+}
+
+.mini-dmm9yo .grabber-3mFHz2 {
+  width: 18px;
+  height: 18px;
+  margin-top: 1.8px;
+}
+
+.iconBadge-2wi9r4.participating-NBGDkr {
+  background-color: #B3B3B3;
+}
+
+.card-2Mz_4z {
+  background-color: rgba(0, 0, 0, 0.5);
+  border-color: #141414;
+}
+
+.selectorButton-EEUWed {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #ffffff !important;
+  outline: 1px solid slategrey;
+}
+
+.replying-1x3H0T:before {
+  background-color: #ffffff;
+}
+
+.replying-1x3H0T {
+  background-color: rgba(0, 0, 0 , .05);
+}
+
+.message-2qnXI6.replying-1x3H0T.selected-2P5D_Z {
+  background-color: rgba(230, 230, 230, .05);
+}
+
+.mouse-mode.full-motion .replying-1x3H0T:hover {
+  background-color: rgba(230, 230, 230, .05);
+}
+
+.replying-1x3H0T:hover {
+  background-color: rgba(230, 230, 230, .05);
+}
+
+.colorStandard-2KCXvj {
+  color: #ffffff;
+}.replying-1x3H0T:hover{
+    background-color: rgba(230, 230, 230, 0.05);
+}
+
+.search-1iTphC .searchBox-2_mAlO .searchBoxInput-K6mkng {
+  color: #ffffff;
+}
+
+.search-1iTphC .searchBox-2_mAlO .closeIcon-2WLZc1 {
+  color: #ffffff;
+}
+
+.colorMuted-HdFt4q {
+  color: #ffffff;
+}
+
+.search-1iTphC .searchBox-2_mAlO .searchIcon-6ljH-4 {
+  color: rgba(255, 255, 255, .6);
+}
+
+.card-3RzMcx {
+  background-color: rgba(17, 17, 17, 0.8)
+}
+
+.card-3RzMcx:hover {
+  background-color: rgba(17, 17, 17, 0.33)
+}
+
+.wrapper-35wsBm {
+  background-color: rgba(17, 17, 17, 0.8);
+  border-radius: 10px;
+}
+
+.side-8zPYf6 .item-PXvHYJ:hover,[style*="color: rgb(114, 137, 218);"]{
+    background-color: rgba(0, 0, 0 , 0.201) !important;
+}
+
+.children-19S4PO {
+    background: rgba(0, 0, 0 , 0.5);
+    border-radius: 20px;
+}
+
+.nicknames-1XK4Zt{
+    color: #ffffff;
+}
+
+.theme-dark .perksModal-fSYqOq{
+    background-color: rgba( 17, 17, 17, .5);
+}
+
+.theme-dark .ctaBar-2UsjF2{
+    background-image: url(https://64.media.tumblr.com/8c56bf39be3210e6b6b37d66a97362d5/08ba3cadbc14dd94-94/s1280x1920/e15388d70debd3dd9b1458e6cc742ededac98a8e.jpg);
+}
+
+.ctaBar-2UsjF2{
+    background-color: #242424 !important;
+}
+
+.barForeground-3KglC8{
+    background-color: #171717;
+}
+
+.tierMarkerAccomplished-2MiXeg,{
+    background-color: #242424;
+}
+
+.tierMarkerLabelPlusIcon-1OS7E9{
+    color: #cccccc;
+}
+
+.anchor-3Z-8Bb{
+    color: #868686;
+}
+
+.theme-dark .icon-QVoPRH, .theme-light .icon-QVoPRH{
+    color: #171717;
+}
+
+.badgeIconWithSubscribers-3PlMu9{
+    color: #171717;
+}
+
+.wrapper-3nSjSv{
+    background: linear-gradient(90deg,#242424,#171717) no-repeat;
+}
+
+.wrapper-3nSjSv:before{
+    background-image: url(https://static.wikia.nocookie.net/tokyo-revengers/images/d/d1/Toman_Symbol.jpg/revision/latest?cb=20200929104257);
+    background-size: 100px 100px;
+}
+
+.cardWarning-2yPNAa{
+  background-color: #242424;
+  border-color: #171717;
+}
+.border-3dQmSY.speaking-WDn8Wm{
+  box-shadow: 0 0 3px 1px rgb(0, 0, 0 ), 0 0 3px 1px rgb(0, 0, 0 ), 0 0 3px 1px rgb(0, 0, 0 );
+}
+svg.homeIcon-FuNwkv{
+    color:transparent;
+}
+.homeIcon-FuNwkv{
+    background-image: url("https://static.wikia.nocookie.net/tokyo-revengers/images/d/d1/Toman_Symbol.jpg/revision/latest?cb=20200929104257");
+    background-size: cover;
+    background-position: center;
+}
+#app-mount .content-1o0f9g{
+    background-color: black;
+}
